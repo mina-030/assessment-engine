01c8e1f3b7a15ccaf7379c01d0ba459d471ec00d
mina-030 <qqmina995@gmail.com>
2025-12-17 00:10:40 -0500

    Initial Maven project with Java 22 and smoke test

---
+++ src/test/java/smoke/SmokeTest.java
@@ -0,0 +1,11 @@
+package smoke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SmokeTest {
+    @Test
+    void projectBuilds() {
+        assertTrue(true);
+    }
+}
